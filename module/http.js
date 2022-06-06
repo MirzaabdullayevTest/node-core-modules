87d9fc3365c177bfe3c2a7d626d93e7feee0d13c
MirzaabdullayevDev <mirzaabdullayev023@gmail.com>
2022-06-06 20:01:38 +0500

    feat: http

--- module/http.js
+++ module/http.js
@@ -1,48 +1,39 @@
 const http = require('http')
+let categories = []
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        if (req.url === '/') {
-            // res.write('Hello from http')
-            res.write('<h1> HEllo from http </h1>')
-            // res.end('<p> This is end function </p>')
-            res.end('<form action="/api/books/add" method="POST"> <label for="name">Book name</label> <br> <input type="text" id="name" name="name"> <br> <input type="submit"> </form>')
-        }
-
-        const books = ['Fact fullness', 'Atomic habits', 'Harry potter']
-
-        if (req.url === '/api/books') {
-            res.write(JSON.stringify(books))
-            res.end()
-        }
+    if (req.url === '/') {
+        res.write('<h1>Bu bosh sahifa</h1>')
+        res.end()
+    } else if (req.url === '/api/categories') {
+        categories = ['Ish', 'Transport', 'Hayvonlar', 'Xizmatlar']
+        res.write(JSON.stringify(categories))
+        res.end()
+    } else if (req.url === '/api/add') {
+        res.write('<h1> Add new category </h1>')
+        res.end('<form action="/api/add/category" method="POST"><label for="name">Category name</label><br> <input type="text" id="name" name="name"> <input type="text" id="id" name="id"> <input type="submit"> </form>')
     }
 
     if (req.method === 'POST') {
-        if (req.url === '/api/books/add') {
-            const body = []
-
+        if (req.url === '/api/add/category') {
+            let body = []
             res.writeHead(200, {
-                'Content-type': 'text/html; charset=utf-8'
+                "Content-type": "text/html; charset=utf-8;"
             })
-
             req.on('data', (data) => {
-                body.push(Buffer.from(data))
+                body = Buffer.from(data).toString()
             })
-
             req.on('end', () => {
-                console.log(body.toString());
-                console.log('END');
-                res.end()
+                res.end('<h1> Sizning post so`rovingiz:</h1> ' + body)
             })
         }
     }
 })
 
 // server.on('connection', (socket) => {
-//     console.log('Connecting port', socket);
+//     console.log('Server connected');
 // })
 
 server.listen(3000, () => {
-    console.log('Server listening on port ', 3000);
+    console.log(`Server working on port ${server.address().port}`);
 })
-
